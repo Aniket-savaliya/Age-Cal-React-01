6f285cf829b87421c7a981a0ecb3ffe40d1f3174
Aniket Savaliya <149785834+Aniket-savaliya@users.noreply.github.com>
2024-10-22 12:07:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AgeCalculator from './Components/AgeCalcuator';
+
+function App() {
+  return (
+    <div className="App">
+           <AgeCalculator/>
+    </div>
+  );
+}
+
+export default App;
